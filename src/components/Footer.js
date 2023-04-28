e22c8e1ecad2731c30d50c8dcfb78c045bb83cc7
tiagokim <kt19go@gmail.com>
2023-04-27 22:35:59 -0700

    Added Header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import './Header.css';
+
+function Header(props) {
+  return (
+    <header>
+      <nav>
+        <Link to="/">HOME</Link>F
+        <Link to="/about">ABOUT</Link>
+        <Link to="/projects">PROJECTS</Link>
+      </nav>
+
+    </header>
+  );
+}
+
+export default Header;
