a1766e5f6b47c45176801def8bbcb0f43485596f
tiagokim <kt19go@gmail.com>
2023-04-27 22:39:42 -0700

    added About, Home, and Projects pages. Also styled and changed App.js

---
+++ src/pages/Projects.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import '../Projects.css'
+
+function Projects(props) {
+  const [projects, setProjects] = useState(null);
+
+  const getProjectsData = async () => {
+    try {
+      const response = await fetch(props.URL + "projects");
+      const data = await response.json();
+      setProjects(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getProjectsData();
+  }, [getProjectsData]);
+  
+
+  const loaded = () => (
+    <div className="projects-container">
+      {projects ? (
+        projects.map((project) => (
+          <div className="project-card" key={project.id}>
+            <h2 className="project-heading">{project.name}</h2>
+            <img className="project-image" src={project.image} alt={project.name} />
+            <a href={project.git}>
+              <button className="project-button">Github</button>
+            </a>
+            <a href={project.live}>
+              <button className="project-button">Live Site</button>
+            </a>
+          </div>
+        ))
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
+
+  return loaded();
+}
+
+export default Projects;
