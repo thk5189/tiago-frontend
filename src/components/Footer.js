046e9e921ebf4333ad9483648681347f92cbfa40
tiagokim <kt19go@gmail.com>
2023-04-28 11:18:27 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,23 @@
-import { Link } from "react-router-dom";
-import './Header.css';
+import '../Footer.css'
 
-function Header(props) {
+function Footer(props) {
   return (
-    <header>
-      <nav>
-        <Link to="/">HOME</Link>F
-        <Link to="/about">ABOUT</Link>
-        <Link to="/projects">PROJECTS</Link>
-      </nav>
-
-    </header>
+    <section className="footer-section">
+     
+      <footer>
+        <div className="social-media-links">
+          <a href="https://www.linkedin.com/in/tiagokim/" target="_blank" rel="noreferrer">
+            <img src="https://i.imgur.com/SAWHX81.png" alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/thk5189" target="_blank" rel="noreferrer">
+            <img src="https://i.imgur.com/2456KHa.png" alt="GitHub" />
+          </a>
+        </div>
+        <p>&copy; {new Date().getFullYear()} Tiago Kim. All rights reserved.</p>
+      </footer>
+    </section>
   );
 }
 
-export default Header;
+export default Footer;
+
