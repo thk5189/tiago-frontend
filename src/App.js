3c56f737cedf578481756ce4a9ea24a7a72e6703
tiagokim <kt19go@gmail.com>
2023-05-03 08:16:14 -0700

    heroku link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Projects from "./pages/Projects";
 function App() {
   
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
-  const URL = "http://localhost:4000/";
+  // const URL = "http://localhost:4000/";
+  const URL = "https://tiago-backend.herokuapp.com/";
 
   return (
     <div className="App">
@@ -25,3 +26,6 @@ function App() {
 }
 
 export default App;
+//styled components for global use ex: gores homes app
+//https://michalsnik.github.io/aos/
+//https://github.com/michalsnik/aos
