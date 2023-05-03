89fe1e91e5ee08890d995977c051b649776b958c
tiagokim <kt19go@gmail.com>
2023-05-02 18:45:08 -0700

    fsadf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,25 @@
-import { Link } from "react-router-dom";
-import './Header.css';
+// import { Link } from "react-router-dom";
+// import './Header.css';
 
 function Header(props) {
   return (
-    <header>
-      <nav>
-        <Link to="/">HOME</Link>
-        <Link to="/about">ABOUT</Link>
-        <Link to="/projects">PROJECTS</Link>
-      </nav>
+    // <header>
+    //   <nav>
+    //     <Link to="/">HOME</Link>
+    //     <Link to="/about">ABOUT</Link>
+    //     <Link to="/projects">PROJECTS</Link>
+    //   </nav>
 
-    </header>
+    // </header>
+      <header class="header">
+      <a href="" class="logo">Tiago.</a>
+      <nav class="navbar">
+          <a href="" class="active">Home</a>
+          <a href="">About</a>
+          <a href="">Projects</a>
+          <a href="">Contact</a>
+      </nav>
+  </header>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,21 +2,25 @@
 // import '../Home.css';
 // import { Link } from "react-router-dom";
 
-
-
 // function Home(props) {
 //   return (
 //     <section className="homeSection">
 //       <div className="header-wrapper">
 //         <img
+//           alt="Headerpic"
 //           src="https://i.imgur.com/t0T4XvR.png"
-//           alt="Header image"
 //           style={{ width: "100%" }}
 //         />
+
 //         <div className="header-text">
-//           <h1>Tiago H. Kim</h1>
+//   <h1>
+//     Tiago H. Kim
+//     <span>
 //           <h2>Software Engineering Student</h2>
+//     </span>
+//   </h1>
 //         </div>
+
 //       </div>
 //       <div className="infoSection">
 //         <p>
@@ -39,62 +43,39 @@
 //           <li>APIs and RESTful services</li>
 //           <li>Version control with Git and GitHub</li>
 //         </ul>
-     
 //       </div>
 //     </section>
 //   );
 // }
 
 // export default Home;
+
 import React from "react";
-import '../Home.css';
-import { Link } from "react-router-dom";
 
 function Home(props) {
   return (
-    <section className="homeSection">
-      <div className="header-wrapper">
-        <img
-          alt="Headerpic"
-          src="https://i.imgur.com/t0T4XvR.png"
-          style={{ width: "100%" }}
-        />
+<section class="home">
+<div class="home-content">
+    <h1><span class="thin">Hello. I am</span> <span class="bold">Tiago Kim</span>.</h1>
 
-        <div className="header-text">
-  <h1>
-    Tiago H. Kim
-    <span>
-          <h2>Software Engineering Student</h2>
-    </span>
-  </h1>
-        </div>
-
-      </div>
-      <div className="infoSection">
-        <p>
-          I am an enthusiastic software engineering student with a strong passion for creating
-          modern and efficient web applications. I have experience in both
-          frontend and backend technologies, and I'm always eager to learn new
-          skills and take on exciting challenges.
-        </p>
-        <h2>Highlights</h2>
-        <p>
-          During my time at General Assembly, I developed and deployed multiple
-          full-stack applications using React, Express, and various APIs. I also
-          collaborated on team <Link to="/projects">projects</Link> and contributed to open-source
-          repositories.
-          </p>
-        <h2>Skills</h2>
-        <ul>
-          <li>HTML, CSS, and JavaScript</li>
-          <li>React, Express, and Node.js</li>
-          <li>APIs and RESTful services</li>
-          <li>Version control with Git and GitHub</li>
-        </ul>
-     
-      </div>
-    </section>
-  );
+  <h3><span class="thin">Software Engineer</span></h3>
+  <p>  I am an enthusiastic software engineer with a strong passion for creating
+    modern and efficient web applications. I have experience in both
+    frontend and backend technologies, and I'm always eager to learn new
+    skills and take on exciting challenges.</p>
+  <div class="btn-box">
+    <a href="#">Hire Me</a>
+    <a href="#">CV</a>
+  </div>
+</div>
+<div class="image-container">
+  {/* <img src="./triangularbird-removebg-preview.png" 
+  alt="Sample image"> */}
+     <img
+          alt="Sample image"
+          src="https://i.imgur.com/DBaeCY1.png"/>
+</div>
+</section>
+)
 }
-
 export default Home;
