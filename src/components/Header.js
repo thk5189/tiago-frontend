950dd1a9826953697d801c0c073785cf43954e3e
tiagokim <kt19go@gmail.com>
2023-05-05 21:54:40 -0700

    added darkmode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,36 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header(props) {
+  // Define the dark mode and light mode images
+  const darkModeImage = "https://i.imgur.com/ID3JsQw.png";
+  const lightModeImage = "https://i.imgur.com/hB2vbiK.png";
+
+  // Define the initial mode state
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Dark mode toggle function
+  function toggleDarkMode() {
+    setIsDarkMode(!isDarkMode);
+    document.body.classList.toggle('dark-mode');
+  }
+
+  // Get the current mode image URL
+  const modeImage = isDarkMode ? darkModeImage : lightModeImage;
+
+  // Define the styles for the mode switch image
+  const modeImageStyles = {
+    cursor: "pointer",
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",
+    boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)",
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
   return (
     <header className="header">
       <Link to="/" className="logo">
@@ -12,15 +42,14 @@ function Header(props) {
         </Link>
         <Link to="/about">About</Link>
         <Link to="/projects">Projects</Link>
-        {/* <Link to="/contact">Contact</Link> */}
       </nav>
+
+      {/* Mode switch image */}
+      <div style={modeImageStyles} onClick={toggleDarkMode}>
+        <img src={modeImage} alt="Mode Switch" style={{ width: "70%" }} />
+      </div>
     </header>
   );
 }
 
 export default Header;
-
-
-
-
-
