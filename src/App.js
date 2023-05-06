eec5a8744f7becc7dae3ddc3e45c9db0b9bf7c00
tiagokim <kt19go@gmail.com>
2023-05-06 01:45:02 -0700

    fadsf

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Hire from "./pages/Hire";
 
